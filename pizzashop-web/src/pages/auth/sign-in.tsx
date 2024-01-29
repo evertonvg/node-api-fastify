7cd783db7b768676033760cf4c8a7a8e2ae8fd8e
Everton <evertoniee@yahoo.com.br>
2024-01-29 17:37:28 -0300

    feat: initial configs

---
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -0,0 +1,8 @@
+import { Helmet } from "react-helmet-async"
+
+export function Signin(){
+    return <>
+        <Helmet title="Login"/>
+        <h1>Signin</h1>
+    </>
+}
---
+++ pizzashop-web/src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter} from "react-router-dom"
+import { Dashboard } from "./pages/app/dashboard"
+import { Signin } from "./pages/auth/sign-in"
+import { AppLayout } from "./pages/_layouts/app"
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout/>,
+        children: [{path: '/',element: <Dashboard/>}]
+    },
+    {
+        path: '/',
+        element: <AppLayout/>,
+        children: [{path: '/sign-in',element: <Signin/>}]
+    }
+    // {
+    //     
+    // },
+    // {
+    //     path: '/sign-in',element: <Signin/>
+    // }
+])
