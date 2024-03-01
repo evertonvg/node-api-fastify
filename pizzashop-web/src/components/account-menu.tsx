fad68c4ebd10199541de88c5b24e8a9c7850aa1b
Everton <evertoniee@yahoo.com.br>
2024-03-01 17:29:25 -0300

    feat:create logout func

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -4,20 +4,35 @@ import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { useMutation } from "@tanstack/react-query";
+import { signOut } from "@/api/sign-out";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 // import { useQuery } from "@tanstack/react-query";
 // import { getProfile } from "@/api/get-profile";
 // import { getManagerRestaurant } from "@/api/get-manager-restaurant";
 // import { Skeleton } from "./ui/skeleton";
 
 export function Accountmenu(){
+    const navigate = useNavigate()
 
     // const { data: profile, isLoading: isLoadingProfile } =useQuery({
     //     queryKey: ['profile'],
     //     queryFn: getProfile
+    //      staleTime: Infinity
     // })
     // const { data: managedRestaurant, isLoading: isLoadingManagerRestaurant } =useQuery({
     //     queryKey: ['managed-restaurant'],
-    //     queryFn: getManagerRestaurant
+    //     queryFn: getManagerRestaurant,
+    //      staleTime: Infinity
+    // })
+
+    // const { mutateAsync: signOutFn, isPending: isSigningOut} = useMutation({
+    //     mutationFn: signOut,
+    //     onSuccess:()=>{
+    //         toast.success('Deslogado com sucesso')
+    //         navigate('/sign-in',{replace:true})
+    //     }
     // })
 
     return (
@@ -71,11 +86,15 @@ export function Accountmenu(){
                             </span>
                         </DropdownMenuItem>
                     </DialogTrigger>
-                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400 flex items-center cursor-pointer">
-                        <LogOut className="mr-2 h-4 w-4" />
-                        <span>
-                            Sair
-                        </span>
+                    <DropdownMenuItem asChild 
+                        // disabled={isSigningOut} 
+                        className="text-rose-500 dark:text-rose-400 flex items-center cursor-pointer">
+                        <button 
+                            // onClick={()=>{signOutFn}} 
+                            className="w-full">
+                            <LogOut className="mr-2 h-4 w-4" />
+                        </button>
+                        
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
--- pizzashop-web/src/components/store-profile-dialog.tsx
+++ pizzashop-web/src/components/store-profile-dialog.tsx
@@ -1,35 +1,79 @@
 import { useForm } from "react-hook-form"
 import { Button } from "./ui/button"
-import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
+import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
 import { z } from "zod"
 import { zodResolver } from '@hookform/resolvers/zod'
+import { GetManagerRestaurantResponse, getManagerRestaurant } from "@/api/get-manager-restaurant"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { updateProfile } from "@/api/update-profile"
+import { toast } from "sonner"
+
+const storeprofileSchema = z.object({
+    name: z.string().min(1),
+    description: z.string().nullable()
+})
+
+type StoreprofileSchema = z.infer<typeof storeprofileSchema>
 
 export function StoreProfileDialog(){
-    // a requisição n irá repetir 
-    // const { data: managedRestaurant, isLoading: isLoadingManagerRestaurant } =useQuery({
-    //     queryKey: ['managed-restaurant'],
-    //     queryFn: getManagerRestaurant
-    // })
+    const queryClient = useQueryClient()
 
-    const storeprofileSchema = z.object({
-        name: z.string().min(1),
-        description: z.string()
+    const { data: managedRestaurant, isLoading: isLoadingManagerRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagerRestaurant,
+        staleTime: Infinity
     })
 
-    type StoreprofileSchema = z.infer<typeof storeprofileSchema>
+    
 
-    // const { register, handleSubmit } = useForm<StoreprofileSchema>({
-    //     resolver: zodResolver(storeprofileSchema),
-    //     values:{
-    //         name: managedRestaurant?.name ?? '',
-    //         description: managedRestaurant?.description ?? ''
-    //     }
-    // })
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<StoreprofileSchema>({
+        resolver: zodResolver(storeprofileSchema),
+        values:{
+            name: managedRestaurant?.name ?? '',
+            description: managedRestaurant?.description ?? ''
+        }
+    })
 
-    const {} = useForm()
+    function updateManagedRestaurantCache({name,description}:StoreprofileSchema){
+        const cached = queryClient.getQueryData<GetManagerRestaurantResponse>(['managedRestaurant'])
+        if(cached){
+            queryClient.setQueryData<GetManagerRestaurantResponse>(['managed-restaurant'],{
+                ...cached,
+                name,description
+            })
+        }
+
+        return {cached}
+    }
+
+    const { mutateAsync: updateProfileFn} = useMutation({
+        mutationFn: updateProfile,
+        onMutate({name,description}){
+            const {cached} = updateManagedRestaurantCache({name,description})
+            return {previousProfile:cached}
+        },
+        onError(_,__,context){
+            if(context?.previousProfile){
+                updateManagedRestaurantCache(context.previousProfile)
+            }
+        }
+    })
+
+    async function handleUpdateProfile(data:StoreprofileSchema) {
+        try {
+            await updateProfileFn({
+                name: data.name,
+                description: data.description
+            })
+
+            toast.success('perfil atualizado com sucesso')
+        } catch {
+            toast.error('Falha ao atualizar o perfil. tente novamente mais tarde!')
+        }
+    }
 
     return <DialogContent> 
             <DialogHeader>
@@ -40,19 +84,19 @@ export function StoreProfileDialog(){
                     Atualize as informações do seu estabelecimento visíveis ao seu cliente
                 </DialogDescription>
             </DialogHeader>
-            <form action="">
+            <form onSubmit={handleSubmit(handleUpdateProfile)}>
                 <div className="space-y-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label className="text-right" htmlFor="name">
                             Nome
                         </Label>
                             <Input className="col-span-3" id="name" 
-                            // {...register('name')} 
+                            {...register('name')} 
                             />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label className="text-right" htmlFor="description" 
-                        // {...register('description')}
+                        {...register('description')}
                         >
                             Descrição
                         </Label>
@@ -60,8 +104,12 @@ export function StoreProfileDialog(){
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button variant="ghost">Cancelar</Button>
-                    <Button variant="success">Salvar</Button>
+                    <DialogClose asChild>
+                        <Button variant="ghost">Cancelar</Button>
+                    </DialogClose>
+                    
+                    <Button variant="success" type="submit" disabled={isSubmitting}>Salvar</Button>
+                    
                 </DialogFooter>
             </form>
         </DialogContent>
